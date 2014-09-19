e0acabd4e2b1bf1d8ff8b56ddc34b1e3403b2c13
n-kats <n-kats19890214@hotmail.co.jp>
2014-09-20 06:27:25 +0900

    introduce rspec tests

---
+++ spec/shiki_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Shiki do
+  it 'has a version number' do
+    expect(Shiki::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end

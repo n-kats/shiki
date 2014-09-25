3da223d7541a32a71f3b965c20137bc5e2885aa8
n-kats <n-kats19890214@hotmail.co.jp>
2014-09-26 04:11:18 +0900

    add some tests
    simple case, many variables case, error case

--- spec/formula_spec.rb
+++ spec/formula_spec.rb
@@ -9,12 +9,32 @@ describe Shiki::Formula do
 
   it 'should parse' do
     ar = Shiki::Formula.parse <<EOS
-hoge:
-  HoGe
+hoge: >
+  HoGe:hige: :hoo
 EOS
     expect(ar).to eq false
   end
 
-  it 'should has README template' do
+  it 'should parse -- array' do
+    ar = Shiki::Formula.parse <<EOS
+hoge:
+  - >
+    {:hige} :hoo : :foo 
+  - [hige, hoo]
+  - hige: T
+    hoo: 3
+EOS
+    expect(ar.first.instance_variable_get(:@variables).length).to eq 3
+    expect(ar.first.variables.length).to eq 3
+  end
+
+  it 'should raise error : illegal order' do
+    yml = <<EOS
+hoge:
+  - >
+    {:a}{:b}{:c}
+  - [a,b,x]
+EOS
+    expect{Shiki::Formula.parse yml}.to raise_error
   end
 end
